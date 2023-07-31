9e3c98e1281c8081dda971fea61dbdfe3570c195
BendySonic <nurbaevdaniar0@gmail.com>
2023-07-31 18:09:58 +0600

    rename/load_system

--- resources/gameScripts/workspace.gd
+++ resources/gameScripts/workspace.gd
@@ -1,53 +1,59 @@
 class_name workspace extends Node2D
 
-const MECH_OBJ_PATH = ["square", "sugaryspire"]
-const OBJ_GRID_PATH = "VBox_1/HBox_2/menu/margin/VBox_1/MarginContainer/Grid"
-const OBJ_MENU_PATH = "VBox_1/HBox_2/menu"
+const MECH_BODY_PATH = ["square_body", "ball_body"]
+const MENU_GRID_PATH = "VBox_1/HBox_2/menu/margin/VBox_1/MarginContainer/Grid"
+const MENU_PATH = "VBox_1/HBox_2/menu"
 
-@onready var canvasLayer = $canvasLayer_UI
-@onready var canvasLayer_choose = $canvasLayer_choose
-var load:Node
-var ui_instance:Node
+var loads:Dictionary
+var ui:Node
+var grid:Node
+
+@onready var layer_ui:CanvasLayer = $LayerUI
+@onready var layer_choose:CanvasLayer = $LayerChoose
+
 
 func _input(event):
 	if event is InputEventMouseButton:
-		for object in ui_instance.get_node(OBJ_GRID_PATH).get_children():
-			if object.is_mouse_inside() and event.is_pressed():
-				var object_cursor = workspace_scenes["menu_object"].instantiate()
-				object_cursor.construct(object.get_info())
+		#gridObject click
+		for grid_object in grid.get_children():
+			if grid_object.is_mouse_inside() and event.is_pressed():
+				var object_cursor = loads["menu_object"].instantiate()
+				object_cursor.construct(grid_object.get_info())
 				object_cursor.set_follow_mouse()
 				
-				canvasLayer_choose.add_child(object_cursor)
-				object_cursor.add_to_group("objectCursor")
+				object_cursor.add_to_group("object_cursor")
+				layer_choose.add_child(object_cursor)
 		
-		for object_cursor in canvasLayer_choose.get_children():
-			if object_cursor.is_in_group("objectCursor"):
+		#objectCurso
+		for object_cursor in layer_choose.get_children():
+			if object_cursor.is_in_group("object_cursor"):
 				if !event.is_pressed():
-					object_cursor.remove_from_group("objectCursor")
-					canvasLayer_choose.remove_child(object_cursor)
-					
-	#obj.position.x = get_global_mouse_position().x - int(get_global_mouse_position().x) % 32
-	#obj.position.y = get_global_mouse_position().y - int(get_global_mouse_position().y) % 32
+					object_cursor.remove_from_group("object_cursor")
+					layer_choose.remove_child(object_cursor)
+
 
 func _ready():
-	workspace_scenes = load.workspace_scenes()
-	
-	ui_instance = workspace_scenes["ui"].instantiate()
-	canvasLayer.add_child(ui_instance)
-	
-	for argName in MECH_OBJ_PATH:
-		var menu_object = workspace_scenes["menu_object"].instantiate()
-		menu_object.construct(argName)
-		ui_instance.get_node(OBJ_GRID_PATH).add_child(menu_object)
+	load_resources()
+	load_scenes()
 
-class loadManager extends Node:
-	func objects():
-		pass
+
+func load_scenes():
+	#ui
+	ui = loads["ui"].instantiate()
+	layer_ui.add_child(ui)
 	
-func workspace_scenes():
-	var loads = {
-		"ui": load("res://resources/gameScenes/workspaceUI.tscn") ,
-		"menu_object": load("res://resources/gameScenes/menuObject.tscn")
+	#grid
+	grid = ui.get_node(MENU_GRID_PATH)
+	
+	#menu_objects
+	for object_name in MECH_BODY_PATH:
+		var object = loads["menu_object"].instantiate()
+		object.construct(object_name)
+		grid.add_child(object)
+
+
+func load_resources():
+	loads = {
+		"ui": preload("res://resources/gameScenes/workspace_ui.tscn") ,
+		"menu_object": preload("res://resources/gameScenes/menu_object.tscn"),
 	}
-	
-	return loads
