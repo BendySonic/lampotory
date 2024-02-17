41780c8d9a6de31c68db3a6aab9d7fc61b325ac2
BendySonic <nurbaevdaniar0@gmail.com>
2024-02-17 23:35:30 +0600

    Added switch

--- src/main/bodies/electrodynamics/switch/switch.gd
+++ src/main/bodies/electrodynamics/switch/switch.gd
@@ -1,2 +1,16 @@
 class_name Switch
 extends FloatNormalBody
+
+var on := false
+
+@onready var animation: AnimationPlayer = get_node("AnimationPlayer")
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			on = not on
+			
+			if on:
+				animation.play("on")
+			else:
+				animation.play_backwards("on")
